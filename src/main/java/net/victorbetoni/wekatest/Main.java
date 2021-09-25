dfd77dd1f042aab8c0f1c19fbae7d267f4ead7bd
Victor <40972803+localthreader@users.noreply.github.com>
2021-09-25 14:51:45 -0300

    Corrigindo dataset

--- src/main/java/net/victorbetoni/wekatest/Main.java
+++ src/main/java/net/victorbetoni/wekatest/Main.java
@@ -8,6 +8,7 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.println(Main.class.getResourceAsStream("/data.arff") == null);
         DataSource data = new DataSource(Main.class.getResourceAsStream("/data.arff"));
         Instances instances = data.getDataSet();
         instances.setClassIndex(3);
@@ -16,6 +17,7 @@ public class Main {
         algorithm.buildClassifier(instances);
 
         Instance entry = new DenseInstance(instances.numAttributes());
+        entry.setDataset(instances);
         entry.setValue(0, "carro");
         entry.setValue(1, "chuvoso");
         entry.setValue(2, "1848");
