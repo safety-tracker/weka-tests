82e693b3fd35f2e93a790338840fe0389807820b
Victor <40972803+localthreader@users.noreply.github.com>
2021-09-25 14:48:52 -0300

    Adicionando data.arff e classe Main para testes

---
+++ src/main/java/net/victorbetoni/wekatest/Main.java
@@ -0,0 +1,27 @@
+package net.victorbetoni.wekatest;
+
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        DataSource data = new DataSource(Main.class.getResourceAsStream("/data.arff"));
+        Instances instances = data.getDataSet();
+        instances.setClassIndex(3);
+
+        NaiveBayes algorithm = new NaiveBayes();
+        algorithm.buildClassifier(instances);
+
+        Instance entry = new DenseInstance(instances.numAttributes());
+        entry.setValue(0, "carro");
+        entry.setValue(1, "chuvoso");
+        entry.setValue(2, "1848");
+
+        double[] chance = algorithm.distributionForInstance(entry);
+        System.out.printf("Y: %f\n", chance[0]);
+        System.out.printf("N: %f\n", chance[1]);
+    }
+}
