ef116d64d493e635b4a6e4c8fd1beb15b51432d2
Victor <40972803+localthreader@users.noreply.github.com>
2021-10-20 14:37:32 -0300

    Implementando loader para datasets .csv

--- src/main/java/net/victorbetoni/wekatest/Generator.java
+++ src/main/java/net/victorbetoni/wekatest/Generator.java
@@ -1,33 +1,46 @@
 package net.victorbetoni.wekatest;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.Random;
 
-public class Generator {
-    public static void main(String[] args) {
-        String[] tipo = new String[]{"carro", "motocicleta", "caminhao"};
-        String[] climas = new String[]{"chuvoso", "tempestade", "limpo", "nevoa"};
-        String[] rodovias = new String[]{"1253", "6235", "4625", "2452", "3424", "2425", "3643"};
-        String[] acidente = new String[]{"Y","N"};
-        File file = new File("src/main/resources/data.arff");
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+public class Generator
+{
+    public static void main(String[] args)
+    {
+        File target = new File("src/main/resources/data.arff");
+        File original = new File("src/main/resources/original.csv");
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(target)); BufferedReader reader = new BufferedReader(new FileReader(original)))
+        {
             writer.write("@relation acidentes\n\n");
-            writer.write("@attribute tipo_veiculo {carro, motocicleta, caminhao}\n");
-            writer.write("@attribute cond_metereologicas {chuvoso, tempestade, limpo, nevoa}\n");
-            writer.write("@attribute rodovia\n");
-            writer.write("@attribute ocorre_acidente {Y,N}\n\n");
+            writer.write("@attribute data_hora_boletim NUMERIC\n");
+            writer.write("@attribute tipo_Veiculo {AUTOMOVEL, BICICLETA, BONDE, CAMINHAO, CAMINHAO-TRATOR, CAMINHONETE, CAMIONETA, CARRO-DE-MAO, CARROCA, CICLOMOTOR, KOMBI, MICROONIBUS, MOTOCICLETA, MOTONETA, ONIBUS, PATINETE, REBOQUE-E-SEMI-REBOQUE, TRATOR-DE-RODAS, TRATOR-MISTO, TRICICLO}\n\n");
+            writer.write("@attribute gravidade STRING");
             writer.write("@data\n");
-            for(int i = 0; i<1000; i++) {
-                String line = String.format("%s, %s, %s, %s\n",
-                        tipo[new Random().nextInt(3)],
-                        climas[new Random().nextInt(4)],
-                        rodovias[new Random().nextInt(rodovias.length)],
-                        acidente[new Random().nextInt(2)]);
-                writer.write(line);
+            String line;
+            boolean first = true;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+            while ((line = reader.readLine()) != null) {
+                if(first) {
+                    first = false;
+                    continue;
+                }
+                String[] vars = line.split(";");
+                LocalDateTime time = LocalDateTime.parse(vars[1], formatter);
+                if(vars[6].contains("INFORMADO")) {
+                    continue;
+                }
+                writer.write(time.getHour() + ", " + vars[6].trim().replace(" ", "-") /*+ "," + vars[vars.length - 1].trim().replace(" ", "-").replace("�", "A")*/ + "\n");
             }
-        } catch (Exception ex) {
+        }
+        catch (Exception ex)
+        {
             ex.printStackTrace();
         }
     }
--- src/main/java/net/victorbetoni/wekatest/Main.java
+++ src/main/java/net/victorbetoni/wekatest/Main.java
@@ -10,19 +10,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         DataSource data = new DataSource(Main.class.getResourceAsStream("/data.arff"));
         Instances instances = data.getDataSet();
-        instances.setClassIndex(3);
 
         NaiveBayes algorithm = new NaiveBayes();
         algorithm.buildClassifier(instances);
-
-        Instance entry = new DenseInstance(instances.numAttributes());
-        entry.setDataset(instances);
-        entry.setValue(0, "carro");
-        entry.setValue(1, "limpo");
-        entry.setValue(2, 1848);
-
-        double[] chance = algorithm.distributionForInstance(entry);
-        System.out.printf("Y: %f\n", chance[0]);
-        System.out.printf("N: %f\n", chance[1]);
     }
 }
